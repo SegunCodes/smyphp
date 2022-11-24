9bc819fb398d1506285d10d0a806fe60637a9452
SegunCodes <shegstix64@gmail.com>
2022-11-24 01:04:01 +0100

    commented out whoops for easy error handling

--- config/app.php
+++ config/app.php
@@ -10,9 +10,9 @@ use SmyPhp\Core\Application;
 use SmyPhp\Core\Authorization\Server;
 
 require_once dirname(__DIR__)."/vendor/autoload.php";
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
+// $whoops = new \Whoops\Run;
+// $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+// $whoops->register();
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 require dirname(__DIR__).'/core/Helpers.php';
