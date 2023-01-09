99b6466be7b2ca645f7532eb3f5a73a3c593bd14
SegunCodes <shegstix64@gmail.com>
2023-01-09 22:31:08 +0100

    added base64 converter as provider

---
+++ app/Providers/Image.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+class Image{
+
+    public function convert($base64Image, $outputFolder, $outputFile) {
+        // Decode the base64-encoded image data
+        $imageData = base64_decode($base64Image);
+
+        if (!file_exists($outputFolder)) {
+            mkdir($outputFolder, 0777, true);
+        }
+      
+        // Create the full output file path
+        $outputFilePath = $outputFolder . '/' . $outputFile;
+      
+        // Create a new image file
+        $imageFile = fopen($outputFilePath, 'w');
+      
+        // Write the decoded image data to the image file
+        fwrite($imageFile, $imageData);
+      
+        // Close the image file
+        fclose($imageFile);
+      
+        return true;
+    }
+      
+      	
+}
