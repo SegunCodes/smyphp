588117a602d0881cb4bb4d1ce2d86af8b60a60fc
SegunCodes <shegstix64@gmail.com>
2023-01-13 09:02:45 +0100

    made changes to base64 image converter

--- app/Providers/Image.php
+++ app/Providers/Image.php
@@ -6,7 +6,15 @@ class Image{
 
     public static function convert($base64Image, $outputFolder, $outputFile) {
         // Decode the base64-encoded image data
-        $imageData = base64_decode($base64Image);
+        $imageData = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64Image));
+        if(!$imageData){
+            return false;
+        }
+        // Create an image from the binary data
+        $img = imagecreatefromstring($imageData);
+        if(!$img){
+            return false;
+        }
 
         if (!file_exists($outputFolder)) {
             mkdir($outputFolder, 0777, true);
@@ -16,7 +24,7 @@ class Image{
         $outputFilePath = $outputFolder . '/' . $outputFile;
       
         // Create a new image file
-        $imageFile = fopen($outputFilePath, 'w');
+        $imageFile = fopen($outputFilePath, 'wb');
       
         // Write the decoded image data to the image file
         fwrite($imageFile, $imageData);
